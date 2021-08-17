ab11bc595b31bf68e352b8160f9086d9f1d37d3d
Cheng-Hong <s26803018@gmail.com>
2021-08-17 14:43:42 +0800

    Initial Commit

---
+++ StoreKit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StoreKit
+//
+//  Created by Cheng-Hong on 2021/8/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
